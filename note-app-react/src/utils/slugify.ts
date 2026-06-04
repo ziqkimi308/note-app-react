6e4b3eee7afff9a11b81f7c4cd16c0ed0b2aeb8b
All Fiction <126734865+ziqkimi308@users.noreply.github.com>
2026-06-04 23:45:50 +0800

    Added some comment into slugify for clarity and understanding.

--- note-app-react/src/utils/slugify.ts
+++ note-app-react/src/utils/slugify.ts
@@ -2,6 +2,6 @@ export function slugify(title: string): string {
 	return title
 		.toLowerCase()
 		.trim()
-		.replace(/[^a-z0-9]+/g, "-")
-		.replace(/(^-|-$)+/g, "");
+		.replace(/[^a-z0-9]+/g, "-") // replace anything not alphabets or numbers to -
+		.replace(/(^-|-$)+/g, ""); // replace - at start or end with blank
 }
