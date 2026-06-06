6531ed3201f80f2bb5515d6a65e434c570bafdb9
All Fiction <126734865+ziqkimi308@users.noreply.github.com>
2026-06-06 23:18:43 +0800

    Added component: Header

---
+++ note-app-react/src/components/Header.tsx
@@ -0,0 +1,16 @@
+import { Link } from "react-router"
+
+export const Header: React.FC = () => {
+	return (
+		<header className="bg-white shadow">
+			<div className="max-w-4xl mx-auto p-4 flex justify-between items-center">
+				<Link to="/" className="text-2xl text-blue-600 font-bold">React Notes App</Link>
+				{/* space-x-3 adds left margin to every child except first one */}
+				<nav className="space-x-3">
+					<Link to="/" className="text-blue-600">Notes</Link>
+					<Link to="/" className="text-blue-600">New</Link>
+				</nav>
+			</div>
+		</header>
+	);
+}
