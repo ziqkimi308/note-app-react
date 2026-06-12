fb7e320d370337ce776692b284e2077dec36a474
All Fiction <126734865+ziqkimi308@users.noreply.github.com>
2026-06-13 00:16:51 +0800

    Progressed NotesList. Finished tags variable using memo hook.

--- note-app-react/src/pages/NotesList.tsx
+++ note-app-react/src/pages/NotesList.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useMemo, useState } from "react";
 
 // type
 type NotesListProps = {
@@ -13,4 +13,17 @@ export const NotesList = ({notes, onDelete}: NotesListProps) => {
 	const [searchQuery, setSearchQuery] = useState<string>("");
 	// tag filter
 	const [tagFilter, setTagFilter] = useState<string | null>(null);
+
+	// Everytime notes changes, tags memo function being executed
+	const tags = useMemo(()=>{
+		// set is data structure a collection of unique values
+		const set = new Set<string>();
+
+		notes.forEach((n) => n.tags.forEach((t) => set.add(t)));
+
+		// array static method which converts iterables data set to real array
+		return Array.from(set);
+	}, [notes]);
+
+	
 }
