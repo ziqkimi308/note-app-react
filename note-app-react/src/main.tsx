66125857904e23904ee1fe41b9b0c199ca0139f7
All Fiction <126734865+ziqkimi308@users.noreply.github.com>
2026-06-13 15:12:53 +0800

    Updated routing methods.

--- note-app-react/src/main.tsx
+++ note-app-react/src/main.tsx
@@ -2,12 +2,17 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import { BrowserRouter } from "react-router";
+import { createBrowserRouter, RouterProvider } from "react-router";
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <App />,
+	},
+]);
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
-		<BrowserRouter>
-			<App />
-		</BrowserRouter>
+		<RouterProvider router={router} />
 	</StrictMode>,
 );
